01b810ef0fba3141451ee1ee58a86f09308276f1
Angel Pena <theangel3535@gmail.com>
2023-12-04 21:24:39 -0500

    add player class

---
+++ src/Player.java
@@ -0,0 +1,16 @@
+import util.Transform;
+
+public class Player {
+    Transform transform;
+
+    public Player(){
+        transform = new Transform(30,300,8,8);
+    }
+    public void update(){
+
+    }
+
+    public void draw(){
+
+    }
+}
